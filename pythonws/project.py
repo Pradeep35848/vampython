e44f9d8e806c8aaa4cc01afac00d6d41591839ce
Pradeep <pp0501317@gmail.com>
2024-11-27 04:18:04 +0530

    updated

--- pythonws/project.py
+++ pythonws/project.py
@@ -1,229 +1,89 @@
-# import mysql.connector as a
-# con=a.connect(host="localhost",user="root",passsword="12345",database="library")
+import os
 
-# def addbook():
-#     bn=input("Enter Book Name:")
-#     c=input("Enter Book Code:")
-#     t=input("Total Books:")
-#     s=input("Enter Subject:")
-#     data=(bn,c,t,s)
-#     sql='insert into books values(%s,%s,%s,%s)'
-#     c=con.cursor()
-#     c.execute(sql,data)
-#     con.commit()
-#     print(">--------------------------------------------------------------------------------------------------------<")
-#     print("Data Entered Successfully")
-#     main()
+class Book:
+    def __init__(self, title, author, isbn, quantity):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.quantity = quantity
 
-#     def issueb():
-#         n=input("Enter Name:")
-#         r=input("Enter Reg No:")
-#         co=input("Enter Book Code:")
-#         d=input("Enter Date:")
-#         a="insert into issue values(%s,%s,%s,%s)"
-#         data=(n,r,co,d)
-#         c=con.cursor()
-#         c.execute(a,data)
-#         con.commit()
-#         print(">-----------------------------------------------------------------------<")
-#         print("Book issued to:",n)
-#         bookup(co,-1)
+    def __str__(self):
+        return f"{self.title} by {self.author} (ISBN: {self.isbn}) - Quantity: {self.quantity}"
 
-#     def submitd():
-#         n=input("Enter Name:")
-#         r=input("Enter Reg No:")
-#         co=input("Enter Book Code:")
-#         d=input("Enter Date:")
-#         a="insert into submit values(%s,%s,%s,%s)"
-#         data=(n,r,co,d)
-#         c=con.cursor()
-#         c.execute(a,data)
-#         con.commit()
-#         print(">-----------------------------------------------------------------------<")
-#         print("Book Submitted from:",n)
-#         bookup(co,1)
+class Library:
+    def __init__(self):
+        self.books = []  
 
-#     def bookup(co,u):
-#         a="select TOTAL from books where BCODE=%s"
-#         data=(co,)
-#         c=con.cursor()
-#         c.execute(a,data)
-#         myresult=c.fetchone()
-#         t=myresult[0]+u
-#         sql="update books set TOTAL =%s where BCODE=%s"
-#         d=(t,co)
-#         c.execute(sql,d)
-#         con.commit()
-#     main()
+    def add_book(self, title, author, isbn, quantity):
+        book = Book(title, author, isbn, quantity)
+        self.books.append(book)
+        print(f"Book '{title}' added successfully!")
 
-#     def dbook():
-#             ac=input("Enter Book Code:")
-#             a="delete frpm books where BCODE =%s"
-#             data=(co,)
-#             c=con.cursor()
-#             c.execute(a,data)
-#             con.commit()
-#     main()
+    def display_books(self):
+        if not self.books:
+            print("No books available in the library.")
+        else:
+            print("\nAvailable Books:")
+            for book in self.books:
+                print(book)
 
+    def borrow_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                if book.quantity > 0:
+                    book.quantity -= 1
+                    print(f"You have borrowed '{book.title}'.")
+                    return
+                else:
+                    print(f"Sorry, '{book.title}' is currently unavailable.")
+                    return
+        print("Book not found in the library.")
 
-#     def  dispbook():
-#          a="select from books"
-#          c=con.cursor()
-#          c.execute(a)
-#          myresult=c.fetchall()
-#          for i in myresult:
-#               print("Book Name:",i[0])
-#               print("Book Code:",i[1])
-#               print("Total:",i[2])
-#               print(">----------------------------------<")
-#     main()
-
-    
-
-#     def main():
-#          print("""
-#                                             LIBRARY MANAGER
-#                1.ADD BOOK
-#                2.ISSUE BOOK 
-#                3.SUBMIT BOOK 
-#                4.DELETE BOOK 
-#                5.DISPLAY BOOKS 
-#                """)
-#          choice=input("Enter Task No:")
-#          print(">----------------------------------------------------<")
-#          if(choice=='1'):
-#             addbook()
-#          elif(choice=='2'):
-#               issueb()
-#          elif(choice=='3'):
-#               submitd()
-#          elif(choice=='4'):
-#               dbook()
-#          elif(choice=='5'):
-#               dispbook()
-#          else:
-#               print("Wrong choice...................")
-#               main()
-    
-#     def pswd():
-#          ps=input("Enter Password:")
-#          if ps=="py143":
-#               main()
-#          else:
-#               print("Wrong Password")
-#               pswd()
-#               pswd()
-
-
-import mysql.connector as a
-
-con = a.connect(host="localhost", user="root", password="12345", database="library")
-
-def addbook():
-    bn = input("Enter Book Name: ")
-    c = input("Enter Book Code: ")
-    t = input("Total Books: ")
-    s = input("Enter Subject: ")
-    data = (bn, c, t, s)
-    sql = 'INSERT INTO books VALUES (%s, %s, %s, %s)'
-    c = con.cursor()
-    c.execute(sql, data)
-    con.commit()
-    print(">--------------------------------------------------------------------------------------------------------<")
-    print("Data Entered Successfully")
-    main()
-
-def issueb():
-    n = input("Enter Name: ")
-    r = input("Enter Reg No: ")
-    co = input("Enter Book Code: ")
-    d = input("Enter Date: ")
-    a = "INSERT INTO issue VALUES (%s, %s, %s, %s)"
-    data = (n, r, co, d)
-    c = con.cursor()
-    c.execute(a, data)
-    con.commit()
-    print(">-----------------------------------------------------------------------<")
-    print("Book issued to:", n)
-    bookup(co, -1)
-
-def submitd():
-    n = input("Enter Name: ")
-    r = input("Enter Reg No: ")
-    co = input("Enter Book Code: ")
-    d = input("Enter Date: ")
-    a = "INSERT INTO submit VALUES (%s, %s, %s, %s)"
-    data = (n, r, co, d)
-    c = con.cursor()
-    c.execute(a, data)
-    con.commit()
-    print(">-----------------------------------------------------------------------<")
-    print("Book Submitted from:", n)
-    bookup(co, 1)
-
-def bookup(co, u):
-    a = "SELECT TOTAL FROM books WHERE BCODE = %s"
-    data = (co,)
-    c = con.cursor()
-    c.execute(a, data)
-    myresult = c.fetchone()
-    t = myresult[0] + u
-    sql = "UPDATE books SET TOTAL = %s WHERE BCODE = %s"
-    d = (t, co)
-    c.execute(sql, d)
-    con.commit()
-
-def dbook():
-    ac = input("Enter Book Code: ")
-    a = "DELETE FROM books WHERE BCODE = %s"
-    data = (ac,)
-    c = con.cursor()
-    c.execute(a, data)
-    con.commit()
-
-def dispbook():
-    a = "SELECT * FROM books"
-    c = con.cursor()
-    c.execute(a)
-    myresult = c.fetchall()
-    for i in myresult:
-        print("Book Name:", i[0])
-        print("Book Code:", i[1])
-        print("Total:", i[2])
-        print(">----------------------------------<")
+    def return_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                book.quantity += 1
+                print(f"You have returned '{book.title}'.")
+                return
+        print("This book was not borrowed from this library.")
 
 def main():
-    print("""
-                                    LIBRARY MANAGER
-               1.ADD BOOK
-               2.ISSUE BOOK
-               3.SUBMIT BOOK
-               4.DELETE BOOK
-               5.DISPLAY BOOKS
-               """)
-    choice = input("Enter Task No: ")
-    print(">----------------------------------------------------<")
-    if(choice == '1'):
-        addbook()
-    elif(choice == '2'):
-        issueb()
-    elif(choice == '3'):
-        submitd()
-    elif(choice == '4'):
-        dbook()
-    elif(choice == '5'):
-        dispbook()
-    else:
-        print("Wrong choice...................")
-        main()
+    library = Library()
 
-def pswd():
-    ps = input("Enter Password: ")
-    if ps == "py143":
-        main()
-    else:
-        print("Wrong Password")
-        pswd()
+    while True:
+        print("\nLibrary Management System")
+        print("1. Add Book")
+        print("2. Display Books")
+        print("3. Borrow Book")
+        print("4. Return Book")
+        print("5. Exit")
+        
+        choice = input("Enter your choice: ")
 
-# Start the program by asking for the password
-pswd()
+        if choice == '1':
+            title = input("Enter book title: ")
+            author = input("Enter book author: ")
+            isbn = input("Enter book ISBN: ")
+            quantity = int(input("Enter quantity: "))
+            library.add_book(title, author, isbn, quantity)
+
+        elif choice == '2':
+            library.display_books()
+
+        elif choice == '3':
+            isbn = input("Enter the ISBN of the book to borrow: ")
+            library.borrow_book(isbn)
+
+        elif choice == '4':
+            isbn = input("Enter the ISBN of the book to return: ")
+            library.return_book(isbn)
+
+        elif choice == '5':
+            print("Exiting the system. Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
